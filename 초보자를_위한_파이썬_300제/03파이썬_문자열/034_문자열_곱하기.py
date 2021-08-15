d8113f6d8208a17883ee218f645519889b7b9443
O2 <shyunb21@gmail.com>
2021-08-15 14:50:05 +0900

    2021-08-15 [파이썬 300제] 31번~40번 풀이 add

---
+++ 초보자를_위한_파이썬_300제/03파이썬_문자열/034_문자열_곱하기.py
@@ -0,0 +1,11 @@
+# 변수에 다음과 같은 문자열이 바인딩되어 있습니다.
+# >>> t1 = 'python'
+# >>> t2 = 'java'
+# 변수에 문자열 더하기와 문자열 곱하기를 사용해서 아래와 같이 출력해보세요.
+# 실행 예: python java python java python java python java
+
+t1 = "pyhon"
+t2 = "java"
+t1_t2 = t1 + " " + t2 + " "
+
+print(t1_t2 * 4)
---
+++ 초보자를_위한_파이썬_300제/03파이썬_문자열/036_문자열_출력.py
@@ -0,0 +1,9 @@
+# 문자열의 format( ) 메서드를 사용해서 035번 문제를 다시 풀어보세요.
+
+name1 = "김민수" 
+age1 = 10
+name2 = "이철희"
+age2 = 13
+
+print("이름: {} 나이: {}".format(name1, age1) )
+print("이름: {} 나이: {}".format(name2, age2) )
