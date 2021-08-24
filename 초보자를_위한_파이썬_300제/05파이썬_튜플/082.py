4eedc6de8034b310597c163937690a28e192b71c
Otwooo <shyunb21@gmail.com>
2021-08-24 22:45:11 +0900

    2021-08-24 [파이썬 300제] 81번~83번 풀이 add

---
+++ 초보자를_위한_파이썬_300제/05파이썬_튜플/082.py
@@ -0,0 +1,8 @@
+# 다음과 같이 10개의 값이 저장된 scores 리스트가 있을 때, start expression을 사용하여 우측 8개의 값을 valid_score 변수에 바인딩하여라.
+
+# scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+
+scores = [8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4]
+_, _, *valid_score = scores
+
+print(valid_score)
