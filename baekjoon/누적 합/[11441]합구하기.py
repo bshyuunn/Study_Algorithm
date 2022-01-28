792f594ae538db681154c0777e0f757680d9259f
Otwooo <shyunb21@gmail.com>
2022-01-28 22:04:30 +0900

    [백준]  11441번 풀이 수정

--- baekjoon/누적 합/[11441]합구하기.py
+++ baekjoon/누적 합/[11441]합구하기.py
@@ -1,16 +1,18 @@
-N1 = int(input()) # 5
+import sys
 
-nums = list(map(int, input().split())) # [10, 20, 30, 40, 50]
+N1 = int(sys.stdin.readline()) # 5
+
+nums = list(map(int, sys.stdin.readline().split())) # [10, 20, 30, 40, 50]
 sums = [nums[0]] #누적합 array # [10]
 
 for i in range(1 ,N1): # 1, 2, 3, 4
     sums.append(sums[-1]+nums[i]) # [10, 30, 60, 100, 150]
     
 
-N2 = int(input())
+N2 = int(sys.stdin.readline())
 
 for i in range(N2):
-    i, j = map(int, input().split())
+    i, j = map(int, sys.stdin.readline().split())
     
     if i == 1:
         print(sums[j-1])
