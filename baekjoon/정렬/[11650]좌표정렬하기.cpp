38d0622f1012df17668c1f679622e07e0716b1a7
Otwooo <shyunb21@gmail.com>
2022-03-23 01:41:24 +0900

    [BOJ] 좌표정렬하기 / 실버5 / 5m

---
+++ baekjoon/정렬/[11650]좌표정렬하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<vector<int>> arr(n, vector <int>(2, 0));    
+    
+    
+    for(int i=0; i<n; i++) {
+        cin >> arr[i][0];
+        cin >> arr[i][1];
+    }
+    sort(arr.begin(), arr.end());
+    
+    for(int i=0; i<n; i++) {
+        cout << arr[i][0] << " " << arr[i][1] << "\n";
+    }
+    
+    return 0;
+}
